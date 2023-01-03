206c03cc9a129bd50d895c8abbe65e9a0545e632
Surya Vedantam <surya.phani1729@gmail.com>
2023-01-03 22:54:01 +0530

    comment moderation added

--- comments/index.js
+++ comments/index.js
@@ -41,10 +41,26 @@ app.post('/posts/:id/comments', async (req, res) => {
 });
 
 
-app.post('/events', (req,res)=>{
+app.post('/events', async (req,res)=>{
 
   console.log('Received Event', req.body.type);
+  const {type,data} = req.body;
 
+  if(type === 'CommentModerated') {
+    const {postId, id, status, content} = data;
+    const comments = commentsByPostId[postId];
+    const comment  = comments.find(comment => {return comment.id === id;});
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {type: 'CommentUpdated', data:{
+      id,
+      status,
+      postId,
+      content
+    }});
+
+
+  }
   res.send({});
 
 });
--- event-bus/index.js
+++ event-bus/index.js
@@ -5,22 +5,37 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
+  // posts
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log("4000-"+err.message);
   });
+  //comments
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log("4001-"+err.message);
   });
+  //query
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log("4002-"+err.message);
+  });
+  //moderation
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log("4003-"+err.message);
   });
   console.log("send response")
   res.send({ status: "OK" });
 });
+
+app.get('/events', (req,res) => {
+  res.send(events);
+});
+
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
